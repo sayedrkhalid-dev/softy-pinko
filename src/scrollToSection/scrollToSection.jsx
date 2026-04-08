bfa8321a03cbdde953813d496d7a3fa54bdd3a1d
sayedrkhalid-dev <sayedrkhalid.official@gmail.com>
2026-04-08 16:07:14 +0600

    Delete all console.log

--- src/scrollToSection/scrollToSection.jsx
+++ src/scrollToSection/scrollToSection.jsx
@@ -1,6 +1,5 @@
 const scrollToSection = (id) => {
   const section = document.getElementById(id);
-  console.log(section);
 
   window.scrollTo({
     top: id === "home" ? 0 : section.offsetTop - 50,
